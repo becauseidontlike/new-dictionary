00cd4eb6ceddb323e86fdb3c805bdce43a572a88
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-03-13 15:19:12 +0100

    search engine added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
         <i className="fa-solid fa-book-open"></i>
+        <main>
+          <Dictionary />
+        </main>
+      <footer className="text-center footer">Coded by WP</footer>
+      </div>
+      
     </div>
   );
 }
