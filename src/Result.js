9bc4c3fe974a339048e4962865a9f84dc543fccf
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-03-16 14:10:04 +0100

    definitions and examples added

--- src/Result.js
+++ src/Result.js
@@ -1,16 +1,19 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Result(props) {
     console.log(props.results);
 
-    if (props.results) {
-    return (
-       <div className="Result">
+if (props.results) {
+return (
+<div className="Result">
         <h2>{props.results.word}</h2>
         {props.results.meanings.map(function(meaning, index) {
-            return meaning.definitions[0].definition;
+            return  <div key={index}>
+                        <Meaning meaning={meaning} />
+                    </div>
         })}
-        </div> 
+</div> 
     )
 } else {
     return null;
