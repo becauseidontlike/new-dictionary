e22b03eaa89b04113cab4789f345585a0c872148
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-03-22 22:01:11 +0100

    buttons for phonetics added

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,11 +1,11 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
 console.log(props.phonetic);
 return (
     <div className="Phonetic">
     <em>{props.phonetic.text}</em>
-    <br />
     <a href={props.phonetic.audio} target="_blank">Listen</a>
     </div>
     )
